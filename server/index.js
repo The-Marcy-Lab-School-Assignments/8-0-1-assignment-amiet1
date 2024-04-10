5b99a83e779680c0b4cd03fb065e0a7b02f050d7
benspector-mls <ben@marcylabschool.org>
2024-04-10 12:38:27 -0400

    code graded: 10/12 tests passing

--- server/index.js
+++ server/index.js
@@ -1,8 +1,17 @@
 const express = require('express');
 const path = require('path');
 
-const pathToDistFolder = path.join(__dirname, '..','path', 'to', 'frontend', 'dist')
+/* FEEDBACK: the path here needs to take you to your vite-project/dist folder. Each argument you pass
+to path.join() is like a step where you `cd`, starting with `__dirname` being the current folder `server/`
 
+So, you'll need to:
+- go up one level to the root of your repo
+- go down into vite-project
+- go down into dist
+
+Once you do that, when you run your server, you should be able to see the built vite project
+*/
+const pathToDistFolder = path.join(__dirname, '..', 'path', 'to', 'frontend', 'dist')
 const app = express();
 
 const logRoutes = (req, res, next) => {
@@ -12,21 +21,21 @@ const logRoutes = (req, res, next) => {
 };
 
 const serveStatic = express.static(pathToDistFolder);
-//*the middleware function for seving static assets 
+//*the middleware function for serving static assets
 
 const serveData = (req, res, next) => {
     const data = [{ name: 'Tahj' }, { age: '21' }, { favColor: 'pink' }];
     res.send(data);
-} 
+}
 
-const serveAbout = (req,res,next) => res.send('<h1>Hello Tahj</h1>')
+const serveAbout = (req, res, next) => res.send('<h1>Hello Tahj</h1>')
 
 app.use(logRoutes);
 app.use(serveStatic);
-app.get('/about',serveAbout);
+app.get('/about', serveAbout);
 app.get('/api/data', serveData);
 
 const port = 8081;
-app.listen(port,() => {
+app.listen(port, () => {
     console.log(`Server is running on http://localhost:${port}`)
 })
